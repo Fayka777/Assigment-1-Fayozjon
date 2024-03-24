305f2f4d097e32bd175740f009e6574f4692a6af
Fayka777 <fayozmuratov777@gmail.com>
2024-03-24 14:14:10 +0900

    Done!!!!

--- Assigment 1.R
+++ Assigment 1.R
@@ -1,2 +1,9 @@
 x<-print ("My first assignment")
 x
+t<- data.frame( "Amir","Altinay","Fayoz","Akbarjon", gender = c("M","F","M","d"), GPA =c (3.2,4.4,4.5,4.5))
+t
+
+
+m<- matrix(1:4, nrow = 2, ncol = 2)
+dimnames(m)<- list(c("a","b"), c("c","d"))
+m
